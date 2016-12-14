03cad26dbd958ab60aa23017928b0999aa1b46c3
Christopher Brown <cebrown5597@gmail.com>
2016-12-13 20:23:53 -0500

    Removed things that should have been removed a long time ago

--- deprecated/clock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Creates a lemonbar clock in right corner
-while :; do
-	echo "%{c}$(date "+%a %d %b %k:%M:%S")%{c}"
-	sleep 1s
-done |
-lemonbar -b -d -g "150x40+1200+16" -f "lemon:pixelsize=11" -B "#f0f0f0" -F "#70838c"
