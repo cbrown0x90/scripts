44a1851faca2a8a997f792ea20c460a457ad83e6
Christopher Brown <cebrown5597@gmail.com>
2021-03-01 23:28:15 -0500

    Update compile-kernel to update-kernel

---
+++ update-kernel.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# get the new kernel version
+NEWVER=$(eselect kernel list | tail -1 | sed -n 's/.*linux-\(.*\)\s.*/\1/p')
+if [ ${#NEWVER} -gt 0 ]; then
+    # delete the old kernel
+    find /boot -maxdepth 1 -type f -name "*" ! -name "*$NEWVER" ! -name "refind_linux.conf" -exec rm -v {} \;
+
+    # build an initramfs
+    genkernel --xfsprogs --luks --lvm --keymap --install initramfs
+
+    # TODO do I need this
+    # update refind config
+    sed -i "s/initramfs-\S*\.img/initramfs-$NEWVER.img/g" "/boot/refind_linux.conf"
+
+    # rebuild externel kernel modules
+    emerge -av --exclude=sys-kernel/gentoo-kernel @module-rebuild
+else
+    echo "Uhh oh, couldn't find the new version for some reason"
+fi
